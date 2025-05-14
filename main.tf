7dbb9c1c68b9393e9e997481ce45c5f16bea61c3
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-05-15 00:48:32 +0530

    creating vpc and ec2 instance using modules

---
+++ main.tf
@@ -0,0 +1,27 @@
+# This is the main Terraform configuration file that sets up the AWS infrastructure.
+
+module "instances" {
+    source = "./EC2_Instance"
+    instance_name = var.instance_name
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    bucket_name = var.bucket_name
+    bucket_key = var.bucket_key
+    bucket_region = var.bucket_region
+    aws_region = var.aws_region
+}
+
+module "vpc" {
+    source = "./VPC"
+    vpc_name = var.vpc_name
+    vpc_cidr = var.vpc_cidr
+    pub_subnet_cidr = var.pub_subnet_cidr
+    pvt_subnet_cidr = var.pvt_subnet_cidr
+    availability_zone = var.availability_zone
+    route_cidr = var.route_cidr
+    filter_name = var.filter_name
+    security_group_name = var.security_group_name
+    pub_subnet_name = var.pub_subnet_name
+    pvt_subnet_name = var.pvt_subnet_name
+}
